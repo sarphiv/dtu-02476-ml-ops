ad4c1eae830bdeefa016516b94b9fdeb849cab45
Lauge <s204111@dtu.dk>
2024-01-06 12:17:45 +0000

    started building the simple pl wrapper to timm models

--- src/ml_backend/__init__.py
+++ src/ml_backend/__init__.py
@@ -1,2 +1,2 @@
-from ml_backend.models.model import MyNeuralNet
+from ml_backend.models.model import BaseModel
 from ml_backend.predict_model import predict
--- src/ml_backend/models/model.py
+++ src/ml_backend/models/model.py
@@ -1,26 +1,59 @@
 import torch
+import pytorch_lightning as pl
+from torch import nn
+from torch.nn import functional as F
+from torch.utils.data import DataLoader, random_split
+from torchvision import transforms
+import typing
 
-class MyNeuralNet(torch.nn.Module):
-    """ Basic neural network class. 
-    
-    Args:
-        in_features: number of input features
-        out_features: number of output features
-    
-    """
-    def __init__(self, in_features: int, out_features: int) -> None:
-        self.l1 = torch.nn.Linear(in_features, 500)
-        self.l2 = torch.nn.Linear(500, out_features)
-        self.r = torch.nn.ReLU()
-    
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """Forward pass of the model.
-        
-        Args:
-            x: input tensor expected to be of shape [N,in_features]
+import timm
 
-        Returns:
-            Output tensor with shape [N,out_features]
-
+class BaseModel(pl.LightningModule):
+    def __init__(self, timm_model, learning_rate):
         """
-        return self.l2(self.r(self.l1(x)))
+        Instantiates a ResNet model from timm library as a pytorch lightning module.
+
+        Parameters:
+        -----------
+        `timm_model`: `timm.models` object
+            the timm model to be used
+        `learning_rate`: `float`
+            learning rate to be used for training
+        
+        """
+        super().__init__()
+        self.save_hyperparameters()
+        self.model = timm_model
+        self.learning_rate = learning_rate
+    
+    def forward(self, x):
+        """
+        Forward pass of the model
+
+        Parameters:
+        -----------
+        `x`: `torch.Tensor` of shape `(batch_size, channels, height, width)`
+            Input tensor to the model
+        """
+        return self.model(x)
+    
+    def _step_helper(self, batch, batch_idx, mode):
+        x, y = batch
+        y_hat = self(x)
+        loss = F.cross_entropy(y_hat, y)
+        self.log(f'{mode}_loss', loss)
+        return loss
+    
+    def training_step(self, batch, batch_idx):
+        return self._step_helper(batch, batch_idx, 'train')
+    
+    def validation_step(self, batch, batch_idx):
+        return self._step_helper(batch, batch_idx, 'val')
+    
+    def test_step(self, batch, batch_idx):
+        return self._step_helper(batch, batch_idx, 'test')
+    
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+        return optimizer
+    

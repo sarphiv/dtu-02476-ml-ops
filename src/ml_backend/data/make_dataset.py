5a5e7bfb9b43e3be27b95aaeb38047b8456f63ff
davidAlvaBro <davidalvabro@gmail.com>
2024-01-06 10:59:39 +0000

    Fixed which folders to use

--- src/ml_backend/data/make_dataset.py
+++ src/ml_backend/data/make_dataset.py
@@ -4,8 +4,8 @@ import torchvision
 def load_dataset():
     """Loads the raw CIFAR10 dataset (either from "data/raw" or the internet) and saves it in "data/processed" folder"""
     # Load / download the datasets and store them in "data/raw" folder 
-    train_dataset = torchvision.datasets.CIFAR10(root="./data/raw", train=True, download=True) 
-    test_dataset = torchvision.datasets.CIFAR10(root="./data/raw", train=False, download=True)
+    train_dataset = torchvision.datasets.CIFAR10(root="./data/raw/CIFAR10", train=True, download=True) 
+    test_dataset = torchvision.datasets.CIFAR10(root="./data/raw/CIFAR10", train=False, download=True)
 
     # Create a transform to make all images tensors 
     transform = torchvision.transforms.ToTensor()
@@ -17,8 +17,8 @@ def load_dataset():
     test_dataset.transform = transform
 
     # Save the datasets in "data/processed" folder
-    torch.save(train_dataset, "./data/processed/train_dataset.pt")
-    torch.save(test_dataset, "./data/processed/test_dataset.pt")
+    torch.save(train_dataset, "./data/processed/CIFAR10/train_dataset.pt")
+    torch.save(test_dataset, "./data/processed/CIFAR10/test_dataset.pt")
 
 
 if __name__ == '__main__':

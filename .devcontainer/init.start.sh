0cec830a19595d618a5ad8be27709a769173978b
davidAlvaBro <davidalvabro@gmail.com>
2024-01-16 13:37:49 +0000

    Update Git and DVC configurations

--- .devcontainer/init.start.sh
+++ .devcontainer/init.start.sh
@@ -2,6 +2,6 @@
 # NOTE: Must run this in start to avoid creating a .gitconfig
 #  after container creation, which prevents copy of local .gitconfig
 echo 'Setting up git...'
-pre-commit install
 git config --global --add safe.directory /workspace
 git config devcontainers-theme.show-dirty 1
+pre-commit install
